78c8ed80464a2491e18bc86d4abb9c5446f9a937
lab192 <lab192@case.edu>
2023-04-24 21:01:32 -0400

    Search works

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from ecommerce_site import views
 from ecommerce_site.models import Listing
 
-home_list_view = views.HomeListView.as_view(
-    queryset= Listing.objects.filter(title__icontains="car").order_by("-time_listed")[:2],  # :5 limits the results to the five most recent
-    context_object_name="post_list",
-    template_name="ecommerce_site/home.html",
-)
-
 urlpatterns = [
     path("", views.home, name="home"),
     path("post/", views.post, name="post"),
